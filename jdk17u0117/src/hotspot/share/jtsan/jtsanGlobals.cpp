0f2ba4a24bf24a2e16ac05b5b6c36b27cb5e23b6
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-04-15 21:42:27 +0300

    add is init global

---
+++ jdk17u0117/src/hotspot/share/jtsan/jtsanGlobals.cpp
@@ -0,0 +1,12 @@
+#include "jtsanGlobals.hpp"
+#include "runtime/atomic.hpp"
+
+bool _is_jtsan_initialized = false;
+
+bool is_jtsan_initialized() {
+    return Atomic::load(&_is_jtsan_initialized);
+}
+
+void set_jtsan_initialized(bool value) {
+    Atomic::store(&_is_jtsan_initialized, value);
+}
--- jdk17u0117/src/hotspot/share/jtsan/jtsanGlobals.hpp
+++ jdk17u0117/src/hotspot/share/jtsan/jtsanGlobals.hpp
@@ -1,16 +1,10 @@
 #ifndef JTSAN_GLOBALS_HPP
 #define JTSAN_GLOBALS_HPP
 
-#include "runtime/atomic.hpp"
+extern bool _is_jtsan_initialized;
 
-bool _is_jtsan_initialized = false;
+bool is_jtsan_initialized(void);
 
-bool is_jtsan_initialized() {
-    return Atomic::load(&_is_jtsan_initialized);
-}
-
-void set_jtsan_initialized(bool value) {
-    Atomic::store(&_is_jtsan_initialized, value);
-}
+void set_jtsan_initialized(bool value);
 
 #endif
