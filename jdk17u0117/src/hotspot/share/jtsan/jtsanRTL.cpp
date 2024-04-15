98d33091fb03f01b664b5191b2a9c14c606e1bc7
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-04-15 19:23:42 +0300

    add is oop check

--- jdk17u0117/src/hotspot/share/jtsan/jtsanRTL.cpp
+++ jdk17u0117/src/hotspot/share/jtsan/jtsanRTL.cpp
@@ -48,10 +48,10 @@ void MemoryAccess(void *addr, Method *m, address &bcp, uint8_t access_size, uint
 
     // FIXME: this is slow
     oop obj = (oopDesc *)addr;
-    if (oopDesc::is_oop(obj) && obj->obj_lock_index()) {
-        fprintf(stderr, "ignoring oop because of set index to %d\n", obj->obj_lock_index());
-        return; // we can ignore locks
-    }
+    // if (oopDesc::is_oop(obj) && obj->obj_lock_index()) {
+    //     fprintf(stderr, "ignoring oop because of set index to %d\n", obj->obj_lock_index());
+    //     return; // we can ignore locks
+    // }
 
     // ResourceMark rm;
 
