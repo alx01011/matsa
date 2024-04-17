b636211ae56821e2d876628305e2b5fc01dea53f
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-04-17 16:47:34 +0300

    attempt to add sync during class initialization

--- jdk17u0117/src/hotspot/share/runtime/thread.hpp
+++ jdk17u0117/src/hotspot/share/runtime/thread.hpp
@@ -719,7 +719,7 @@ class JavaThread: public Thread {
   bool           _in_asgct;                      // Is set when this JavaThread is handling ASGCT call
   bool           _on_thread_list;                // Is set when this JavaThread is added to the Threads list
   OopHandle      _threadObj;                     // The Java level thread object
-  bool           _initializing_class;            // Is set when this JavaThread is initializing a class
+  bool           _initializing_class = false;            // Is set when this JavaThread is initializing a class
 
 #ifdef ASSERT
  private:
