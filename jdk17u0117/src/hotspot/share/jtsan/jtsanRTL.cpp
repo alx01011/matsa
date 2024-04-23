51fcb9cee3f8f028b1a13a6b44a6f0c34c533948
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-04-24 02:14:15 +0300

    add log on access

--- jdk17u0117/src/hotspot/share/jtsan/jtsanRTL.cpp
+++ jdk17u0117/src/hotspot/share/jtsan/jtsanRTL.cpp
@@ -94,7 +94,9 @@ void MemoryAccess(void *addr, Method *m, address &bcp, uint8_t access_size, bool
 
     int lineno = m->line_number_from_bci(m->bci_from(bcp));
     if (lineno == 30 || lineno == 26) {
-        fprintf(stderr, "Accessing memory (%lu), at line %d by thread %d\n", (uptr)addr, lineno, tid);
+        fprintf(stderr, "\t\tAccessing memory (%lu), at line %d by thread %d\n", (uptr)addr, lineno, tid);
+        fprintf(stderr, "\t\tShadow addr: %p\n", ShadowMemory::MemToShadow((uptr)addr));
+
     }
 
     // race
