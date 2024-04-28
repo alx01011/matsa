44dec8f4c5d26539497b82448c55976acce731f1
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-04-28 03:39:30 +0300

    add transfer epoch functions that do not increment vector clocks

--- jdk17u0117/src/hotspot/share/jtsan/jtsanRTL.cpp
+++ jdk17u0117/src/hotspot/share/jtsan/jtsanRTL.cpp
@@ -38,6 +38,13 @@ bool CheckRaces(uint16_t tid, void *addr, ShadowCell &cur, ShadowCell &prev) {
 }
 
 void MemoryAccess(void *addr, Method *m, address &bcp, uint8_t access_size, bool is_write) {
+    int lineno = m->line_number_from_bci(m->bci_from(bcp));
+    if (lineno == 39 || lineno == 33) {
+        ResourceMark rm;
+        fprintf(stderr, "MemoryAccess: %s, %d by tid %d\n", m->external_name_as_fully_qualified(), lineno, tid);
+    }
+
+
     // if jtsan is not initialized, we can ignore
     if (!is_jtsan_initialized()) {
         return;
@@ -54,12 +61,6 @@ void MemoryAccess(void *addr, Method *m, address &bcp, uint8_t access_size, bool
     uint32_t epoch = JtsanThreadState::getEpoch(tid, tid);
     // create a new shadow cell
     ShadowCell cur = {tid, epoch, (uint8_t)((uptr)addr & (8 - 1)), get_gc_epoch(), is_write};
-
-    int lineno = m->line_number_from_bci(m->bci_from(bcp));
-    if (lineno == 39 || lineno == 33) {
-        ResourceMark rm;
-        fprintf(stderr, "MemoryAccess: %s, %d by tid %d\n", m->external_name_as_fully_qualified(), lineno, tid);
-    }
 
     // race
     ShadowCell prev;
