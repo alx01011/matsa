7fa2eaccfe2ed96c978db5c0119d5d6deeb3bb14
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-04-29 16:34:40 +0300

    bound check in vectorclock =

--- jdk17u0117/src/hotspot/share/jtsan/vectorclock.cpp
+++ jdk17u0117/src/hotspot/share/jtsan/vectorclock.cpp
@@ -1,12 +1,24 @@
 #include "vectorclock.hpp"
 
+#include <stdio.h>
+
 uint64_t& Vectorclock::operator[](size_t index) {
     return this->_clock[index];
 }
 
 Vectorclock& Vectorclock::operator=(const Vectorclock& other) {
     for (size_t i = 0; i < other._slot_size; i++) {
+        if (other._slot_size >= MAX_THREADS) {
+            fprintf(stderr, "Slot size %zu out of bounds of 1024\n", other._slot_size);
+        }
+
         size_t index = other._slot[i];
+
+        if (index >= MAX_THREADS) {
+            fprintf(stderr, "Index %zu out of bounds of 1024\n", index);
+        }
+
+
         this->set(index, other._clock[index]);
     }
 
