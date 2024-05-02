be2b088b4965ee95118034d6f9da683c86e9d355
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-05-02 19:57:09 +0300

    add tests

---
+++ jdk17u0117/jtsan_tests/tests/no_race/nr_arrays.java
@@ -0,0 +1,38 @@
+public class nr_arrays {
+    double[] darr = new double[20];
+
+    public static void main(String... args) {
+        int[] iarr = new int[20];
+
+        Thread t1 = new Thread(() -> {
+            increment(iarr, 1);
+            increment(iarr, 3);
+
+            increment(darr, 1);
+            increment(darr, 3);
+        });
+
+        Thread t2 = new Thread(() -> {
+            increment(iarr, 2);
+            increment(iarr, 4);
+
+            increment(darr, 2);
+            increment(darr, 4);
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void increment(int[] arr, int index) {
+        arr[index]++;
+    }
+}
