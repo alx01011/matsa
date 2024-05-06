a285bfb4cb56b71ed4156d764190df0f05fd1173
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-05-06 03:24:53 +0300

    add more racy tests

--- jdk17u0117/jtsan_tests/tests/race/r_different_obj_locks.java
+++ jdk17u0117/jtsan_tests/tests/race/r_different_obj_locks.java
@@ -1,16 +1,16 @@
 public class r_different_obj_locks {
     public static int x = 3;
-    
+
     public static void main(String... args) {
         Object lock1 = new Object();
         Object lock2 = new Object();
 
         Thread t1 = new Thread(() -> {
-            increment1(lock1, x);
+            increment1(lock1);
         });
 
         Thread t2 = new Thread(() -> {
-            increment2(lock2, y);
+            increment2(lock2);
         });
 
         t1.start();
