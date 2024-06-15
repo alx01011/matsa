e4ee46a3d04581661473ced9af1faac83261ae95
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-06-16 02:04:05 +0300

    add suppressions

--- jdk17u0117/src/hotspot/share/jtsan/suppression.cpp
+++ jdk17u0117/src/hotspot/share/jtsan/suppression.cpp
@@ -1,4 +1,99 @@
 #include "suppression.hpp"
 
-// TODO: add options to get suppression file from command line
+
+void Trie::insert(const char *name) {
+    TrieNode* current = root;
+
+    const char *str = name;
+    for (char c = *str; c != '\0'; c = *(++str)) {
+        if (c == '*') {
+            current->has_wildcard = true;
+            break;
+        }
+
+        if (current->children.find(c) == current->children.end()) {
+            current->children[c] = new TrieNode();
+        }
+        current = current->children[c];
+    }
+    current->is_end_of_word = true;
+}
+
+bool Trie::search(const char *name) {
+    TrieNode* current = root;
+
+    // if root has just a wildcard and no other children then it matches everything aka "*"
+    if (current->has_wildcard && current->children.size() == 0) {
+        return true;
+    }
+
+    const char *str = name;
+    for (char c = *str; c != '\0'; c = *(++str)) {
+        // not a children of the current node
+        if (current->children.find(c) == current->children.end()) {
+            if (current->has_wildcard) {
+                return true;
+            }
+            return false;
+        }
+        current = current->children[c];
+    }
+
+    return current->is_end_of_word;
+}
+
+static void add_suppressions(Trie *trie, const char *suppression_string) {
+    // assume each string can have up to 1023 characters
+    char buffer[1024] = "";
+
+    for (size_t i = 0, j = 0; suppression_string[i]; i++) {
+        if (suppression_string[i] == '\n') {
+            buffer[j] = 0;
+
+            trie->insert(buffer);
+            buffer[j = 0] = 0;
+        } else {
+            if (j >= 1023) {
+                fprintf(stderr, "Suppression string too long\n");
+                exit(1);
+            }
+
+            buffer[j++] = suppression_string[i];
+        }
+    }
+}
+
+Trie *JTSanSuppression::top_frame_suppressions = nullptr;
+Trie *JTSanSuppression::frame_suppressions     = nullptr;
+
+void JTSanSuppression::init(void) {
+    top_frame_suppressions = new Trie();
+    frame_suppressions     = new Trie();
+
+    add_suppressions(top_frame_suppressions, def_top_frame_suppressions);
+    add_suppressions(frame_suppressions    , def_frame_suppressions);
+}
+
+bool JTSanSuppression::is_suppressed(JTSanStackTrace *stack_trace) {
+    // first check the top frame
+    const char *frame = stack_trace->get_frame(0).method->external_name_as_fully_qualified();
+    if (top_frame_suppressions->search(frame)) {
+        return true;
+    }
+
+    // now check the rest of the frames
+    for (size_t i = 1; i < stack_trace->frame_count(); i++) {
+        frame = stack_trace->get_frame(i).method->external_name_as_fully_qualified();
+        if (frame_suppressions->search(frame)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+
+
+
 
--- jdk17u0117/src/hotspot/share/jtsan/suppression.hpp
+++ jdk17u0117/src/hotspot/share/jtsan/suppression.hpp
@@ -1,19 +1,49 @@
 #ifndef SUPPRESSION_HPP
 #define SUPPRESSION_HPP
 
-const char * const def_top_frame_suppressions[] = {
+#include "stacktrace.hpp"
+
+#include "memory/allocation.hpp"
+
+#include <unordered_map>
+
+class TrieNode {
+public:
+    std::unordered_map<char, TrieNode*> children;
+    bool is_end_of_word;
+    bool has_wildcard;
+
+    TrieNode() : is_end_of_word(false), has_wildcard(false) {}
+};
+
+class Trie {
+    private:
+        TrieNode* root;
+
+    public:
+        Trie();
+
+        void insert(const char *name);
+        bool search(const char *name);
+};
+
+
+const char * def_top_frame_suppressions = {
     ""
 };
 
-const char * const def_frame_suppressions[] = {
-    "java.lang.invoke.*",
-    "java.util.concurrent.locks.*"
+const char * def_frame_suppressions = {
+    "java.lang.invoke.*\n"
+    "java.util.concurrent.locks.*\n"
 };
 
-class JTSanSuppression {
+class JTSanSuppression : public AllStatic, public CHeapObj<mtInternal> {
     public:
         static void init();
-        
+        static bool is_suppressed(JTSanStackTrace *stack_trace);
+    private:
+        static Trie *top_frame_suppressions;
+        static Trie *frame_suppressions;
 };
 
 
