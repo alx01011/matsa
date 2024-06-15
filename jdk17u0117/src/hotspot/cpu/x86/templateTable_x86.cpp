aa04d3cd6dc20fa0ec4c57be9a15cfbd76b29da3
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-06-15 21:15:23 +0300

    ignore movl for flags in fast store

--- jdk17u0117/src/hotspot/cpu/x86/templateTable_x86.cpp
+++ jdk17u0117/src/hotspot/cpu/x86/templateTable_x86.cpp
@@ -3572,11 +3572,11 @@ void TemplateTable::fast_storefield(TosState state) {
   __ jcc(Assembler::zero, notVolatile);
 
     // get flags
-  JTSAN_ONLY(
-    __ movl(rdx, Address(rcx, rbx, Address::times_ptr,
-                  in_bytes(base +
-                              ConstantPoolCacheEntry::flags_offset())));
-  );
+  // JTSAN_ONLY(
+  //   __ movl(rdx, Address(rcx, rbx, Address::times_ptr,
+  //                 in_bytes(base +
+  //                             ConstantPoolCacheEntry::flags_offset())));
+  // );
 
   fast_storefield_helper(field, rax);
   volatile_barrier(Assembler::Membar_mask_bits(Assembler::StoreLoad |
@@ -3584,11 +3584,11 @@ void TemplateTable::fast_storefield(TosState state) {
   __ jmp(Done);
   __ bind(notVolatile);
 
-  JTSAN_ONLY(
-    __ movl(rdx, Address(rcx, rbx, Address::times_ptr,
-                  in_bytes(base +
-                              ConstantPoolCacheEntry::flags_offset())));
-  );
+  // JTSAN_ONLY(
+  //   __ movl(rdx, Address(rcx, rbx, Address::times_ptr,
+  //                 in_bytes(base +
+  //                             ConstantPoolCacheEntry::flags_offset())));
+  // );
 
   fast_storefield_helper(field, rax);
 
