9e7e1979afb3e32a34c78525954c42a0375d17b0
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-06-28 01:34:03 +0300

    try to optimize stacktrace

--- jdk17u0117/src/hotspot/share/jtsan/stacktrace.cpp
+++ jdk17u0117/src/hotspot/share/jtsan/stacktrace.cpp
@@ -1,29 +1,29 @@
 #include "stacktrace.hpp"
 
-static frame next_frame(frame fr, Thread* t) {
-  // Compiled code may use EBP register on x86 so it looks like
-  // non-walkable C frame. Use frame.sender() for java frames.
-  frame invalid;
-  if (t != nullptr && t->is_Java_thread()) {
-    // Catch very first native frame by using stack address.
-    // For JavaThread stack_base and stack_size should be set.
-    if (!t->is_in_full_stack((address)(fr.real_fp() + 1))) {
-      return invalid;
-    }
-    if (fr.is_java_frame() || fr.is_native_frame() || fr.is_runtime_frame()) {
-      RegisterMap map(t->as_Java_thread(), false); // No update
-      return fr.sender(&map);
-    } else {
-      // is_first_C_frame() does only simple checks for frame pointer,
-      // it will pass if java compiled code has a pointer in EBP.
-      if (os::is_first_C_frame(&fr)) return invalid;
-      return os::get_sender_for_C_frame(&fr);
-    }
-  } else {
-    if (os::is_first_C_frame(&fr)) return invalid;
-    return os::get_sender_for_C_frame(&fr);
-  }
-}
+// static frame next_frame(frame fr, Thread* t) {
+//   // Compiled code may use EBP register on x86 so it looks like
+//   // non-walkable C frame. Use frame.sender() for java frames.
+//   frame invalid;
+//   if (t != nullptr && t->is_Java_thread()) {
+//     // Catch very first native frame by using stack address.
+//     // For JavaThread stack_base and stack_size should be set.
+//     if (!t->is_in_full_stack((address)(fr.real_fp() + 1))) {
+//       return invalid;
+//     }
+//     if (fr.is_java_frame() || fr.is_native_frame() || fr.is_runtime_frame()) {
+//       RegisterMap map(t->as_Java_thread(), false); // No update
+//       return fr.sender(&map);
+//     } else {
+//       // is_first_C_frame() does only simple checks for frame pointer,
+//       // it will pass if java compiled code has a pointer in EBP.
+//       if (os::is_first_C_frame(&fr)) return invalid;
+//       return os::get_sender_for_C_frame(&fr);
+//     }
+//   } else {
+//     if (os::is_first_C_frame(&fr)) return invalid;
+//     return os::get_sender_for_C_frame(&fr);
+//   }
+// }
 
 JTSanStackTrace::JTSanStackTrace(Thread *thread) {
   if (thread == nullptr) {
