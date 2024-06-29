c334b8e9b6e73a170817320b168f818b32fdbc98
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-06-29 20:33:49 +0300

    fix report string

--- jdk17u0117/src/hotspot/share/jtsan/report.cpp
+++ jdk17u0117/src/hotspot/share/jtsan/report.cpp
@@ -71,14 +71,14 @@ void JTSanReport::do_report_race(JTSanStackTrace *trace, void *addr, uint8_t siz
     fprintf(stderr, "==================\n");
 
     fprintf(stderr, RED "WARNING: ThreadSanitizer: data race (pid=%d)\n", pid);
-    fprintf(stderr, BLUE " %s of size %u at %p by thread T%u:\n" RESET,  cur.is_write ? "Write" : "Read", 
+    fprintf(stderr, BLUE " %s of size %u at %p by thread T%lu:\n" RESET,  cur.is_write ? "Write" : "Read", 
             size, addr, cur.tid);
     if (!try_print_event_trace(addr, cur.tid)) {
         // less accurate line numbers
         print_stack_trace(trace);
     }
     
-    fprintf(stderr, BLUE "\n Previous %s of size %u at %p by thread T%u:\n" RESET, prev.is_write ? "write" : "read", 
+    fprintf(stderr, BLUE "\n Previous %s of size %u at %p by thread T%lu:\n" RESET, prev.is_write ? "write" : "read", 
             size, addr, prev.tid);
     if (!try_print_event_trace(addr, prev.tid)) {
         fprintf(stderr, "  <no stack trace available>\n");
