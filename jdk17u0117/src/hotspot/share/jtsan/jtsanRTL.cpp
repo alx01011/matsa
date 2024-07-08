de71523bc61319002779c192a2c01f8fb5e03323
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-07-08 22:24:04 +0300

    fully vectorize check races

--- jdk17u0117/src/hotspot/share/jtsan/jtsanRTL.cpp
+++ jdk17u0117/src/hotspot/share/jtsan/jtsanRTL.cpp
@@ -18,6 +18,85 @@
 #include "oops/oop.inline.hpp"
 #include "utilities/decoder.hpp"
 
+// bool JtsanRTL::CheckRaces(JavaThread *thread, JTSanStackTrace* &trace, void *addr, ShadowCell &cur, ShadowCell &prev) {
+//     void *shadow_addr = ShadowMemory::MemToShadow((uptr)addr);
+
+//     m256 shadow        = _mm256_load_si256((__m256i*)shadow_addr);
+//     m256 thread_epochs = _mm256_setzero_si256();
+
+//     // load cur
+//     const m256 cur_cell = _mm256_insert_epi64(zero, *(uint64_t)&cur, 0);
+
+//     const m256 zero = _mm256_setzero_si256();
+//     const m256 one  = _mm256_set1_epi64x(1);
+
+//     const m256 tid        = _mm256_and_si256(shadow, _mm256_set1_epi64x(0xFF));
+//     const m256 offset     = _mm256_and_si256(_mm256_srli_epi64(shadow, 40), _mm256_set1_epi64x(0x7));
+//     const m256 gc_epoch   = _mm256_and_si256(_mm256_srli_epi64(shadow, 43), _mm256_set1_epi64x(0x7FFFF));
+//     const m256 is_write   = _mm256_and_si256(_mm256_srli_epi64(shadow, 62), _mm256_set1_epi64x(0x1));
+//     const m256 is_ignored = _mm256_and_si256(_mm256_srli_epi64(shadow, 63), _mm256_set1_epi64x(0x1));
+
+//     const m256 is_ignored = _mm256_cmpeq_epi64(is_ignored, one);
+//     // ignored bit set in one of the cells
+//     if (_mm256_movemask_epi8(is_ignored)) {
+//         return false;
+//     }
+
+//     const m256 epoch = _mm256_and_si256(_mm256_srli_epi64(shadow, 8), _mm256_set1_epi64x(0xFFFFFFFF));
+
+//           // tid == current.id
+//     m256 skip_mask = _mm256_cmpeq_epi64(tid, _mm256_set1_epi64x(cur.tid));
+//          // r/r
+//          skip_mask = _mm256_or_si256(skip_mask, _mm256_or_si256(is_write, _mm256_set1_epi64x(cur.is_write)));
+//          // offset != cur.offset
+//          skip_mask = _mm256_or_si256(skip_mask, _mm256_cmpeq_epi64(offset, _mm256_set1_epi64x(cur.offset)));
+//          // gc_epoch != cur.gc_epoch
+//          skip_mask = _mm256_or_si256(skip_mask, _mm256_cmpeq_epi64(gc_epoch, _mm256_set1_epi64x(cur.gc_epoch)));
+
+//         shadow = _mm256_andnot_si256(skip_mask, shadow);
+
+// #define LOAD_EPOCH(idx)\
+//     {\
+//         uint8_t tid    = _mm256_extract_epi8(tid, idx * 8);\
+//         uint32_t epoch = JTSanThreadState::getEpoch(cur.tid, tid);\
+//         thread_epochs = _mm256_insert_epi32(thread_epochs, epoch, idx);\
+//     }
+
+//     LOAD_EPOCH(0);
+//     LOAD_EPOCH(1);
+//     LOAD_EPOCH(2);
+//     LOAD_EPOCH(3);
+
+// #undef LOAD_EPOCH
+
+//     // thr >= cell.epoch
+//     const m256 cmp = _mm256_cmpgt_epi32(thread_epochs, epoch);
+//     // if thr >= cell.epoch then skip
+//     const m256 report_mask = _mm256_andnot_si256(_mm256_cmpeq_epi64(zero, shadow), cmp);
+
+//     int report = _mm256_movemask_epi8(report_mask);
+
+//     // race
+//     if (report) {
+//         int idx = __builtin_ffs(report) / 8;
+
+//         prev.tid      = _mm256_extract_epi8(tid, idx * 8);
+//         prev.epoch    = _mm256_extract_epi32(epoch, idx);
+//         prev.offset   = _mm256_extract_epi8(offset, idx * 8);
+//         prev.gc_epoch = _mm256_extract_epi32(gc_epoch, idx);
+//         prev.is_write = _mm256_extract_epi8(is_write, idx * 8);
+
+//         fprintf(stderr, "data race!");
+
+//         return true;
+//     }
+
+
+
+//     return false;
+
+// }
+
 
 #if !JTSAN_VECTORIZE
 bool JtsanRTL::CheckRaces(JavaThread *thread, JTSanStackTrace* &trace, void *addr, ShadowCell &cur, ShadowCell &prev) {
@@ -94,6 +173,7 @@ bool JtsanRTL::CheckRaces(JavaThread *thread, JTSanStackTrace* &trace, void *add
 
 #else
 
+/*
 bool JtsanRTL::CheckRaces(JavaThread *thread, JTSanStackTrace* &trace, void *addr, ShadowCell &cur, ShadowCell &prev) {
     void *shadow_addr = ShadowMemory::MemToShadow((uptr)addr);
 
@@ -164,6 +244,7 @@ bool JtsanRTL::CheckRaces(JavaThread *thread, JTSanStackTrace* &trace, void *add
 
     return isRace;
 }
+*/
 
 #endif
 
