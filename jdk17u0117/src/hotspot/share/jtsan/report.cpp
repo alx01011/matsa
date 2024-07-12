e404badbc132a0d67e0b547c2040cd7a692b7230
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-07-12 03:53:38 +0300

    type pun

--- jdk17u0117/src/hotspot/share/jtsan/report.cpp
+++ jdk17u0117/src/hotspot/share/jtsan/report.cpp
@@ -49,7 +49,7 @@ bool try_print_event_trace(void *addr, int tid, ShadowCell &prev) {
 
     if (has_trace) {
         for (int i = trace.size - 1; i >= 0; i--) {
-            JTSanEvent e = *(JTSanEvent*)&trace.events[i];
+            JTSanEvent e = trace.events[i];
 
             Method *m       = (Method*)e.pc;
             if (!Method::is_valid_method(m)) {
