a98b3696bf17f694332356836e61a6485077b491
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-07-17 13:05:39 +0300

    fix branch

--- jdk17u0117/src/hotspot/share/jtsan/symbolizer.cpp
+++ jdk17u0117/src/hotspot/share/jtsan/symbolizer.cpp
@@ -14,7 +14,7 @@ ThreadHistory::ThreadHistory() {
     events            = (uint64_t*)os::reserve_memory(EVENT_BUFFER_SIZE * sizeof(uint64_t));
     event_shadow_addr = (void*)os::reserve_memory(EVENT_BUFFER_SIZE * sizeof(void*));
 
-    if (!events || !event_epoch) {
+    if (!events || !event_shadow_addr) {
         fatal("JTSan Symbolizer: Failed to mmap");
     }
 
