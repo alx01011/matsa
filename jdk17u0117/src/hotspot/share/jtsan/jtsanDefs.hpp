98325398c3189bbca078f99c1f367dbb330b79ba
Alex Antonakakis <antonakakis1@gmail.com>
2024-07-31 19:02:23 +0300

    check if builtin_expect is available

--- jdk17u0117/src/hotspot/share/jtsan/jtsanDefs.hpp
+++ jdk17u0117/src/hotspot/share/jtsan/jtsanDefs.hpp
@@ -44,7 +44,12 @@ typedef __m256i m256;
 
 // branch prediction
 
+#ifdef __GNUC__
 #define LIKELY(x)   __builtin_expect(!!(x), 1)
 #define UNLIKELY(x) __builtin_expect(!!(x), 0)
+#else
+#define LIKELY(x)   (x)
+#define UNLIKELY(x) (x)
+#endif
 
 #endif
