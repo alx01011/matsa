a0d8b2bea9a6cb4113218d62bfd9d1e9022c5a3a
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-08-04 22:27:54 +0300

    add jvm option

--- jdk17u0117/build_jtsan.sh
+++ jdk17u0117/build_jtsan.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Usage:
-# ./build_jtsan.sh -fd (fast debug build)
-# ./build_jtsan.sh -sd (slow debug build)
-# ./build_jtsan.sh -r (release build) (default)
+# ./build_jtsan.sh -fd --boot-jvm <JVM17> (fast debug build)
+# ./build_jtsan.sh -sd --boot-jvm <JVM17> (slow debug build)
+# ./build_jtsan.sh --boot-jvm <JVM17> (release build) (default)
 
 CONFIGURE_FLAGS='--disable-warnings-as-errors '
 JTSAN_BUILD_CXXFLAGS='--with-extra-cxxflags=-DINCLUDE_JTSAN'
+BOOT_JVM=''
 CONF='linux-x86_64-server-release'
 
 # check for debug builds
@@ -18,6 +19,17 @@ elif [ "$1" == "-sd" ]; then
     CONFIGURE_FLAGS='--disable-warnings-as-errors --with-debug-level=slowdebug'
     CONF='linux-x86_64-server-slowdebug'
     shift
+fi
+
+# check for boot jvm
+if [ "$1" == "--boot-jvm" ]; then
+    BOOT_JVM=$2
+    shift
+fi
+
+# check if boot-jvm has been set so we can include in flags
+if [ -n "$BOOT_JVM" ]; then
+    CONFIGURE_FLAGS="$CONFIGURE_FLAGS --with-boot-jdk=$BOOT_JVM"
 fi
 
 if [ ! -f configure ]; then
