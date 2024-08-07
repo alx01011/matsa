ff71fe1fdc02bb17b13cc6c2aea6564f6459c216
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-08-07 15:37:13 +0300

    revert branch prediction hints

--- jdk17u0117/src/hotspot/share/jtsan/jtsanDefs.hpp
+++ jdk17u0117/src/hotspot/share/jtsan/jtsanDefs.hpp
@@ -45,10 +45,8 @@ typedef __m256i m256;
 // branch prediction
 
 #ifdef __GNUC__
-// #define LIKELY(x)   __builtin_expect(!!(x), 1)
-// #define UNLIKELY(x) __builtin_expect(!!(x), 0)
-#define LIKELY(x)   (x)
-#define UNLIKELY(x) (x)
+#define LIKELY(x)   __builtin_expect(!!(x), 1)
+#define UNLIKELY(x) __builtin_expect(!!(x), 0)
 
 #define ALIGNED(x) __attribute__((aligned(x)))
 
