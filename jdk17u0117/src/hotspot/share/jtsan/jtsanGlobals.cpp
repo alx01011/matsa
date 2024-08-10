0e485ec8cd2b4a60231c89a2b7257b30bf3ef3dd
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-08-10 03:17:00 +0300

    remove counters

--- jdk17u0117/src/hotspot/share/jtsan/jtsanGlobals.cpp
+++ jdk17u0117/src/hotspot/share/jtsan/jtsanGlobals.cpp
@@ -8,9 +8,6 @@ volatile uint32_t _gc_epoch = 0;
 
 volatile bool   _is_klass_init = false;
 
-volatile uint64_t _func_entry_counter = 0;
-volatile uint64_t _memory_access_counter = 0;
-
 // start from 1 since 0 is reserved for the initial java thread
 volatile uint16_t _cur_tid = 1;
 
@@ -56,25 +53,6 @@ void decrement_tid(void) {
     uint16_t tid = Atomic::load(&_cur_tid);
     Atomic::store(&_cur_tid, (uint16_t)(tid - 1));
 }
-
-void increment_func_entry(void) {
-    Atomic::inc(&_func_entry_counter);
-}
-
-uint64_t get_func_entry(void) {
-    // no need for atomic only exiting thread will read this
-    return _func_entry_counter;
-}
-
-void increment_memory_access(void) {
-    Atomic::inc(&_memory_access_counter);
-}
-
-uint64_t get_memory_access(void) {
-    return _memory_access_counter;
-}
-
-
 
 #define COUNTER_DEF(x)\
     uint64_t JTSanStats::x##_counter = 0;
