d9011e72d4aded9d7cd1cc06aff78d6886df50a3
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-10-07 13:47:48 +0300

    add clinit test

---
+++ jdk17u0117/jtsan_tests/cl_init.java
@@ -0,0 +1,34 @@
+class baz {
+    public static int foo;
+
+    static {
+        foo = 42;
+    }
+
+    public static int getFoo() {
+        return foo;
+    }
+}
+
+class bar {
+    public static int foo;
+
+    static {
+        foo = baz.getFoo();
+    }
+}
+
+
+public class cl_init {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            bar a = new bar();
+            System.out.println(baz.foo);
+        });
+
+        Thread t2 = new Thread(() -> {
+            baz a = new baz();
+            System.out.println(bar.foo);
+        });
+    }
+}
