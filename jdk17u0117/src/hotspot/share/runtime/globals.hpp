ea7796c5c67f4f631f1010f5f72232c2575bd0a8
Alex Antonakakis <antonakakis1@gmail.com>
2024-11-11 14:32:45 +0200

    lower default history size to 4

--- jdk17u0117/src/hotspot/share/runtime/globals.hpp
+++ jdk17u0117/src/hotspot/share/runtime/globals.hpp
@@ -2105,7 +2105,7 @@ const intx ObjectAlignmentInBytes = 8;
           range(0, UINT_MAX)                                                \
   product(bool, MaTSa, false, "Enable Managed Thread Sanitizer.")              \
   product(bool, MaTSaSilent, false, "Silent mode for Managed Thread Sanitizer.") \
-  product(uint, MaTSaHistorySize, 6, "Size of the history buffer for Managed Thread Sanitizer.") \
+  product(uint, MaTSaHistorySize, 4, "Size of the history buffer for Managed Thread Sanitizer.") \
         range(1, 16)\
 
 // end of RUNTIME_FLAGS
