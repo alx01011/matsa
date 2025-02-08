07d140a941c431341a40c1c88ea161977c3e1c4d
Alexandros Antonakakis <antonakakis1@gmail.com>
2025-02-08 05:38:19 +0200

    test compiler instrumentation

--- jdk17u0117/src/hotspot/share/matsa/matsaRTL.cpp
+++ jdk17u0117/src/hotspot/share/matsa/matsaRTL.cpp
@@ -121,6 +121,6 @@ void MaTSaRTL::MemoryAccess(void *addr, Method *m, address &bcp, uint8_t access_
 }
 
 void MaTSaRTL::matsa_should_be_called(void) {
-    putchar('%');
+    fputc('%', stderr);
     return; // dummy function
 }
