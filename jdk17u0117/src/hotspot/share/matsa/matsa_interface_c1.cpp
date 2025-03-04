6a65f7541d83c1590c40125f5323337106f95e5f
Alexandros Antonakakis <antonakakis1@gmail.com>
2025-03-04 16:57:26 +0200

    add synchronized enter/exit interface

--- jdk17u0117/src/hotspot/share/matsa/matsa_interface_c1.cpp
+++ jdk17u0117/src/hotspot/share/matsa/matsa_interface_c1.cpp
@@ -4,6 +4,7 @@
 #include "history.hpp"
 #include "threadState.hpp"
 #include "lockState.hpp"
+#include "oops/oop.inline.hpp"
 
 #include <cstdint>
 
@@ -59,30 +60,30 @@ void (*MaTSaC1::matsa_array_access[2][9])(void *addr, int idx, BasicType array_t
 };
 
 void MaTSaC1::method_enter(JavaThread *thread, Method *method) {
-    int tid = JavaThread::get_matsa_tid(current);
+    int tid = JavaThread::get_matsa_tid(thread);
 
-    MaTSaStack *stack = JavaThread::get_matsa_stack(current);
+    MaTSaStack *stack = JavaThread::get_matsa_stack(thread);
     uint16_t bci = stack->get_caller_bci();
     // first 48 bits are the method id, last 16 bits are the bci
     uint64_t packed_frame = ((uint64_t)method << 16) | (uint64_t)bci;
 
     // Symbolizer::Symbolize(FUNC, method, bci, tid);
     stack->push(packed_frame);
-    History::add_event(current, method, bci);
+    History::add_event(thread, method, bci);
 }
 
 void MaTSaC1::method_exit(JavaThread *thread) {
-    int tid = JavaThread::get_matsa_tid(current);
+    int tid = JavaThread::get_matsa_tid(thread);
     // assume 0,0 means method exit
     // Symbolizer::Symbolize(FUNC, 0, 0, tid);
-    MaTSaStack *stack = JavaThread::get_matsa_stack(current);
+    MaTSaStack *stack = JavaThread::get_matsa_stack(thread);
     (void)stack->pop();
   
     History::add_event(current, 0, 0);
 }
 
-void MaTSaC1::pre_method_enter(JavaThread *current, Method *method, int bci) {
-    MaTSaStack *stack = JavaThread::get_matsa_stack(current);
+void MaTSaC1::pre_method_enter(JavaThread *thread, Method *method, int bci) {
+    MaTSaStack *stack = JavaThread::get_matsa_stack(thread);
     // set the caller bci to be used by next func enter
     stack->set_caller_bci((uint16_t)bci);
 }
